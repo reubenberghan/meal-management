66559fa44c7a62644d571658c6743fb10bc0df14
Reuben Berghan <reuben.berghan@gmail.com>
2018-11-13 07:44:34 +1300

    Add some initial state

--- src/state/constants.js
+++ src/state/constants.js
@@ -10,4 +10,55 @@ export const SIGN_IN_PATH = '/sign-in'
 export const TERMS_OF_USE_PATH = '/terms-of-use'
 
 // State
-export const initialState = {}
+export const initialState = {
+  recipes: [
+    {
+      id: '123',
+      title: 'Banana Cake',
+      ingredients: [
+        {
+          id: 'abc',
+          name: 'Banana'
+        },
+        {
+          id: 'abd',
+          name: 'Flour'
+        }
+      ],
+      steps: [
+        {
+          id: 'abe',
+          content: 'Peel banana'
+        },
+        {
+          id: 'abf',
+          content: 'Mix ingredients'
+        }
+      ]
+    },
+    {
+      id: '124',
+      title: 'Toasted Sandwich',
+      ingredients: [
+        {
+          id: 'abg',
+          name: 'Bread'
+        },
+        {
+          id: 'abh',
+          name: 'Cheese'
+        }
+      ],
+      steps: [
+        {
+          id: 'abi',
+          content: 'Slice cheese'
+        },
+        {
+          id: 'abj',
+          content: 'Toast bread'
+        }
+      ]
+    }
+  ]
+}
